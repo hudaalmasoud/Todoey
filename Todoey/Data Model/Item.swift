423c1682433e43d6297d8162fc21301f86124f40
hmasoud <hudamasoud86@gmail.com>
2019-02-28 18:19:07 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Huda Al Masoud on 2/28/19.
+//  Copyright © 2019 Huda Al Masoud. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
